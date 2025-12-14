cf91506dc3839274898ea5d13967882179f83c11
MALIKKHUBAIB1 <khubaib9221@gmail.com>
2025-12-14 19:33:36 +0530

    all changes done

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()], // ✅ React plugin add kiya
-  server: {
-    proxy: {
-      "/api": "http://localhost:5000",
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
     },
   },
 });
